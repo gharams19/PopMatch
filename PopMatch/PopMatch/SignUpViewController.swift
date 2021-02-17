3932955b9d2cf9423ad62ab7032df9ac7369f4f7
Eden <avivi.eden@gmail.com>
2021-02-16 16:30:03 -0800

    verify password and added comments on code

--- PopMatch/PopMatch/SignUpViewController.swift
+++ PopMatch/PopMatch/SignUpViewController.swift
@@ -20,35 +20,49 @@ class SignUpViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         errLabel.text = nil
+        
+        //password is automatically hidden
         createPassTextField.isSecureTextEntry = true
         verifyPassTextField.isSecureTextEntry = true
         
     }
     
     func toggleButtonTitle(between titles:(String, String), on button: UIButton) -> Void {
+        
+        //toggle between the two titles given
         let newTitle = button.currentTitle == titles.0 ? titles.1 : titles.0
         button.setTitle(newTitle, for: .normal)
     }
     
     @IBAction func showCreatePass() {
+        
+        //switch between two titles, show and hide
         toggleButtonTitle(between: showHideTitles, on: showHideCreate)
         if showHideCreate.title(for: .normal) == "show" {
+            //show the password
             createPassTextField.isSecureTextEntry = true
         } else if showHideCreate.title(for: .normal) == "hide" {
+            //hide the password
             createPassTextField.isSecureTextEntry = false
         }
     }
     
     @IBAction func showVerifyPass() {
+        
+        //switch between two titles, show and hide
         toggleButtonTitle(between: showHideTitles, on: showHideVerify)
         if showHideVerify.title(for: .normal) == "show" {
+            //show the password
             verifyPassTextField.isSecureTextEntry = true
         } else if showHideVerify.title(for: .normal) == "hide" {
+            //hide the password
             verifyPassTextField.isSecureTextEntry = false
         }
     }
 
     @IBAction func logInHere() {
+        
+        //Go into the log in viewcontroller, no checks needed
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let logInViewController = storyboard.instantiateViewController(withIdentifier: "logInVC") as? ViewController else {
             assertionFailure("couldn't find vc") //will stop program
@@ -58,15 +72,26 @@ class SignUpViewController: UIViewController {
         navigationController?.pushViewController(logInViewController, animated: true)
     }
     @IBAction func signUpButton() {
-        //check if the email is correct and also have a check to verify the password
-        //change back button to login viewController
+        //check if the email is correct
         
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        guard let mainViewController = storyboard.instantiateViewController(withIdentifier: "mainVC") as? MainViewController else {
-            assertionFailure("couldn't find vc") //will stop program
-            return
+        if createPassTextField.text != verifyPassTextField.text {
+            
+            //if password fields do not match, present error
+            errLabel.textColor = .red
+            errLabel.text = "Password does not match"
+        } else {
+            
+            //empty error
+            errLabel.text = nil
+            
+            //go into next view controller
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            guard let mainViewController = storyboard.instantiateViewController(withIdentifier: "mainVC") as? MainViewController else {
+                assertionFailure("couldn't find vc") //will stop program
+                return
+                }
+            //optional navigation controller
+            navigationController?.pushViewController(mainViewController, animated: true)
         }
-        //optional navigation controller
-        navigationController?.pushViewController(mainViewController, animated: true)
     }
 }
--- PopMatch/PopMatch/ViewController.swift
+++ PopMatch/PopMatch/ViewController.swift
@@ -19,26 +19,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         errLabel.text = nil
+        
+        //password is automatically hidden
         passwordTextField.isSecureTextEntry = true
     }
     func toggleButtonTitle(between titles:(String, String), on button: UIButton) -> Void {
+        
+        //toggle between the two titles given
         let newTitle = button.currentTitle == titles.0 ? titles.1 : titles.0
         button.setTitle(newTitle, for: .normal)
     }
     
     @IBAction func showPassword() {
+        
+        //switch between two titles, show and hide
         toggleButtonTitle(between: showHideTitles, on: showHideButton)
         if showHideButton.title(for: .normal) == "show" {
+            //show the password
             passwordTextField.isSecureTextEntry = true
         } else if showHideButton.title(for: .normal) == "hide" {
+            //hide the password
             passwordTextField.isSecureTextEntry = false
         }
-        
-        
             
     }
     
     @IBAction func signUpHere() {
+        
         //move to the signUpVC
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let signUpViewController = storyboard.instantiateViewController(withIdentifier: "signUpVC") as? SignUpViewController else {
@@ -53,6 +60,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func logInButton() {
+        
         //check if the email and password match to an account in the database, if it matches
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         guard let mainViewController = storyboard.instantiateViewController(withIdentifier: "mainVC") as? MainViewController else {
@@ -61,9 +69,12 @@ class ViewController: UIViewController {
         }
         //optional navigation controller
         navigationController?.pushViewController(mainViewController, animated: true)
+        
+        //clear textfields and dismiss keyboard
         emailTextField.text = nil
         passwordTextField.text = nil
         self.view.endEditing(true)
+        
         //if it does not match, present error label on the type of error 
     }
     
