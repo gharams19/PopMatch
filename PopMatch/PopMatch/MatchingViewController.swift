195649f9b29860ef52519ece19be62d69e63c5a0
Gharam Alsaedi <gbalsaedi@ucdavis.edu>
2021-02-24 18:12:26 -0800

    popUp VC

--- PopMatch/PopMatch/MatchingViewController.swift
+++ PopMatch/PopMatch/MatchingViewController.swift
@@ -12,9 +12,19 @@ class MatchingViewController: UIViewController {
     @IBOutlet weak var matchUsername: UILabel!
     @IBOutlet weak var matchImage: UIImageView!
     
+    
+    
+    @IBOutlet weak var popUpView: UIView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var rejectButton: UIButton!
+    @IBOutlet weak var acceptButton: UIButton!
+    
+    let matchName = ""
+    var rejectedMatches = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        matchUsername.text = nil
+        //matchUsername.text = nil
         
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
@@ -28,6 +38,12 @@ class MatchingViewController: UIViewController {
 
         matchImage.addGestureRecognizer(imageTap)
         matchImage.isUserInteractionEnabled = true
+        
+        popUpView.layer.borderWidth = 2.5
+        popUpView.layer.borderColor = UIColor(displayP3Red: 1.0, green: 0.54, blue: 0.11, alpha: 1.0).cgColor
+          
+        popUpView.isHidden = true
+     
         
     }
     
@@ -47,16 +63,48 @@ class MatchingViewController: UIViewController {
             switch swipeGesture.direction {
             case .right:
                 print("Swiped right")
+                acceptMatch()
             case .left:
                 print("Swiped left")
+                rejectMatch()
             default:
                 break
             }
         }
     }
     
-    @objc func imageTapped() {
-        print("image Tapped")
+    func acceptMatch() {
+        /*Check if match accepted too*/
+        //if yes, go to meetingVC
+        
+        //if no, go to lobbyVC
     }
     
+    func rejectMatch() {
+        /*Add user to rejectedMatches array*/
+        
+        /*Go to lobbyVC to find another */
+        
+    }
+    @IBAction func invokeAcceptMatchFunc(_ sender: Any) {
+        acceptMatch()
+    }
+    
+    @IBAction func invokeRejectMatchFunc(_ sender: Any) {
+        rejectMatch()
+    }
+    
+    @objc func imageTapped() {
+        popUpView.isHidden = false
+        backButton.isUserInteractionEnabled = false
+        acceptButton.isUserInteractionEnabled = false
+        rejectButton.isUserInteractionEnabled = false
+    }
+    
+    @IBAction func closePopUpView(_ sender: Any) {
+        popUpView.isHidden = true
+        backButton.isUserInteractionEnabled = true
+        acceptButton.isUserInteractionEnabled = true
+        rejectButton.isUserInteractionEnabled = true
+    }
 }
